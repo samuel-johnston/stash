916501d8dd2b7839347d87c4ee115d3ea1ea9cb2
Samuel Johnston <sbj04@outlook.com>
2024-12-19 19:02:40 +1100

    Added name field + fixed makers

--- forge.config.ts
+++ forge.config.ts
@@ -1,27 +1,23 @@
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 
 import { rendererConfig } from "./webpack/webpack.renderer";
 import { mainConfig } from "./webpack/webpack.main";
 
 const config: ForgeConfig = {
   packagerConfig: {
+    name: "Stash",
     asar: true,
     extraResource: [
       "./src/assets/data",
     ],
   },
-  rebuildConfig: {},
   makers: [
-    {
-      name: "@electron-forge/maker-squirrel",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-dmg",
-      config: {},
-    },
+    new MakerSquirrel(),
+    new MakerDMG(),
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
