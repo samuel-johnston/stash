7346c661a295c174d4a42df188630218accb6303
Samuel Johnston <sbj04@outlook.com>
2024-12-20 18:10:13 +1100

    Added updated icons/logos

--- forge.config.ts
+++ forge.config.ts
@@ -1,23 +1,34 @@
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerDMG } from "@electron-forge/maker-dmg";
-
 import { rendererConfig } from "./webpack/webpack.renderer";
 import { mainConfig } from "./webpack/webpack.main";
+import path from "path";
 
 const config: ForgeConfig = {
   packagerConfig: {
     name: "Stash",
     asar: true,
+    icon: "./src/assets/icons/app_icon",
     extraResource: [
       "./src/assets/data",
     ],
   },
   makers: [
-    new MakerSquirrel(),
-    new MakerDMG(),
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        iconUrl: path.join(__dirname, "/src/assets/icons/installer_icon.ico"),
+        setupIcon: path.join(__dirname, "/src/assets/icons/installer_icon.ico"),
+        skipUpdateIcon: true,
+      },
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        icon: path.join(__dirname, "/src/assets/icons/installer_icon.icns"),
+      }
+    },
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
